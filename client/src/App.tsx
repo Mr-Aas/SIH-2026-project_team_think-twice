0a706b218a50e30c603382fe8a7c51c4fea7b16c
Aas Mohd <mraasmohd1234@gmail.com>
2026-08-27 18:04:10 +0530

    client initial interface

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container bg-red-300 min-w-full min-h-screen rounded-2xl flex justify-center items-center font-bold text-black'>
+        its is just beggining
+
+      </div>
+    </>
+  )
+}
+
+export default App
